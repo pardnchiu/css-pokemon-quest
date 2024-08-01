e2bbfb0dc52d55ed948658ea1682707e5bfa52fe
pardnchiu <dev@pardn.io>
2024-08-02 00:26:47 +0800

    202408020026

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(function () {
+
+    var delay_observer;//, bottom_observer;
+
+    document.addEventListener("DOMContentLoaded", function () {
+
+        document.body.scrollTop = document.body.clientHeight
+    });
+}());
